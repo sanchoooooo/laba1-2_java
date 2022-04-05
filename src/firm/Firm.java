f32ad5e736b2fdb756b0ee23b7fc53e029b845b3
sanchoooooo <79538544+sanchoooooo@users.noreply.github.com>
2022-04-05 19:10:31 +0300

    Update Firm.java

--- src/firm/Firm.java
+++ src/firm/Firm.java
@@ -1,2 +1,40 @@
-package firm;public class Firm {
+package firm;
+
+import java.util.Scanner;
+
+public class Firm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in, "cp1251");
+        System.out.println("Введите количество сотрудников =>");
+        int kol = sc.nextInt();
+        sc.nextLine(); // очистка буфера после ввода числа
+
+        firm.Sotrudnik[] sotr = new firm.Sotrudnik[kol];// получена ссылка на массив сотрудников
+        System.out.println("Введите информацию о каждом сотруднике: ");
+
+        for (int i = 0; i < sotr.length; i++) {
+            sotr[i] = new firm.Sotrudnik();
+            // Присвоение значений полям
+            System.out.print("Введите фамилию" + (i + 1) + " сотрудника => ");
+            sotr[i].fam = sc.nextLine();
+            System.out.print("Введите его имя => ");
+            sotr[i].im = sc.nextLine();
+            System.out.print("Введите его отчество => ");
+            sotr[i].otch = sc.nextLine();
+            System.out.print("Введите его должность => ");
+            sotr[i].doljnost = sc.nextLine();
+            System.out.print("Введите его оклад => ");
+            sotr[i].oklad = sc.nextInt();
+            sc.nextLine();// очистка буфера
+        } // end for
+
+        // Вывод информации о сотрудниках
+        System.out.println("\n Сотрудники фирмы: \n фам \t имя \t отч  +\t должность \tОклад");
+
+
+        for (firm.Sotrudnik s : sotr) {
+            System.out.println(s.fam + "\t" + s.im + "\t" + s.otch
+                    + "\t " + s.doljnost + "\t\t" + s.oklad);
+        }
+    }
 }
